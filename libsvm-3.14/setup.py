d57cb267323ac5a12e2f4808f4cf130aa7154b71
Salniger <tojima.t.1101@gmail.com>
2013-01-21 01:59:31 +0900

    Extract tar.

---
+++ libsvm-3.14/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "libsvm",
+    version = "3.14",
+    py_modules = ['svm','svmutil'],
+    author = "Salinger",
+    description = "This is libsvm buindings for Python with Homebrew.",
+    license = "The modified BSD license",
+    url = "https://github.com/Salinger/libsvm-python",
+)
